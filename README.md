7e538072eed186388432694087903eb800a8df7c
MichalMazurek11 <mazurek041198@wp.pl>
2022-12-12 22:34:33 +0100

    Poprawa kodu, zmiana sposobu wyszukiwanai pilkow

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 The title of the project: game
-The date and time at which make was run:  30.10.22 19:40:19
-The number of lines of code contained in guessinggame.sh:  24
+The date and time at which make was run: 
+The number of lines of code contained in guessinggame.sh:  0
--- guessinggame.sh
+++ guessinggame.sh
@@ -3,12 +3,16 @@
 Con () {
 	echo "Congratulations!"
 }
-number_files="$(ls -pa |grep -v /|wc -l)" 
+#FOR ALL FILES ls -Al |grep '^[-dlspDc]'|wc -l
+number_files="$(ls -Al |grep '^[-dlspDc]'|wc -l)" 
+#FOR REGULAR TYPE FILES
+#number_files="$(ls -al |grep '^-')"
 check_if_number='^[0-9]+$'
+to_quess=-1
 while [[ $to_quess -ne $number_files ]]
 do
 	echo -n "How many files are in the current directory?: " 
-	read to_quess
+	read  to_quess
 	if [[ $to_quess =~ $check_if_number ]]
 	then
 		if [[ $to_quess -gt $number_files ]]
@@ -19,6 +23,9 @@ do
 		then
 			echo "more"
 		fi
+	else
+		to_quess=-1
+		echo "Enter a number"
 	fi
 done
 Con
